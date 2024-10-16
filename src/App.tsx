e42bbb90c4713a4510b2658a0c263e14acfd2eff
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-16 14:55:06 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Todolist } from './Todolist';
-import { Task } from './Todolist';
+
+
+export type FilterTypes = 'All' | 'Active' |'Completed'
+
 
 function App() {
-    const title1 = 'What to learn-1'
-    const title2 = 'What to learn-2'
-    const title3 = 'What to learn-3'
+    const title = 'What to learn-1'
 
-    const tasks1:Task[] = [
+    // let tasks:Task[] = [
+    //     {id: 1, title: 'HTML&CSS', isDone: true},
+    //     {id: 2, title: 'JS', isDone: true},
+    //     {id: 3, title: 'React', isDone: false},
+    //     {id: 1, title: 'Redux', isDone: true},
+    //     {id: 2, title: 'Typescript', isDone: false},
+    //     {id: 3, title: 'RTX query', isDone: false},
+    // ]
+
+    //   покажи, положи и перерисуй
+    let [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Redux', isDone: true},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTX query', isDone: false},
     ]
+    )
 
-    const tasks2:Task[] = [
-        {id: 1, title: 'Hello world', isDone: true},
-        {id: 2, title: 'I am happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
+    const [valueForFilter, setValueForFilter] = useState('All');
 
-    const tasks3:Task[] = []
+    const removeTask = (taskId: number) => {
+        // tasks = tasks.filter((el) => el.id !== taskId)
+        // setTasks(tasks)
+
+        setTasks(tasks.filter((el) => el.id !== taskId))
+    }
+
+    const filterTasks = (filterValue: FilterTypes) => {
+        setValueForFilter(filterValue)
+    }
+
+    let durshlag = tasks;
+    if (valueForFilter === 'Active') durshlag = tasks.filter((el) => !el.isDone)
+    if (valueForFilter === 'Completed') durshlag = tasks.filter((el) => el.isDone)
+
 
     return (
         <div className="App">
-            <Todolist title={title1} tasks={tasks1}/>
-            <Todolist title={title2} tasks={tasks2}/>
-            <Todolist title={title3} tasks={tasks3}/>
+            <Todolist
+                title={title}
+                tasks={durshlag}
+                removeTask={removeTask}
+                filterTasks={filterTasks}
+            />
         </div>
     );
 }
