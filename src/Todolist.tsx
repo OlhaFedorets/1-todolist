c8ba67251352ac2ac882f1774313005f401008e1
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-16 17:08:22 +0300

    change todolist

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FilterTypes} from "./App";
 
 type Props = {
     title: string
     tasks: Task[]
     removeTask: (taskId: number) => void
-    filterTasks: (filterValue: FilterTypes) => void
+    // filterTasks: (filterValue: FilterTypes) => void
 }
 
 export type Task = {
@@ -14,34 +14,65 @@ export type Task = {
     isDone: boolean
 }
 
-export const Todolist = ({ title, tasks, removeTask, filterTasks }: Props) => {
+export const Todolist = ({title, tasks, removeTask}: Props) => {
+
+    const [valueForFilter, setValueForFilter] = useState('All');
+
+    const filterTasks = (filterValue: FilterTypes) => {
+        setValueForFilter(filterValue)
+    }
+
+    const filteredTasks = () => {
+        // let durshlag = tasks;
+        // if (valueForFilter === 'Active') durshlag = tasks.filter((el) => !el.isDone)
+        // if (valueForFilter === 'Completed') durshlag = tasks.filter((el) => el.isDone)
+        // return durshlag
+
+
+        // то же самое через switch
+        let durshlag = tasks;
+        switch (valueForFilter) {
+            case 'Active': {
+                return durshlag = tasks.filter((el) => !el.isDone)
+            }
+            case 'Completed': {
+                return durshlag = tasks.filter((el) => el.isDone)
+            }
+        }
+        return durshlag
+
+    }
+
+    const funCompleted = filteredTasks()
+
 
     return (
         <div>
             <h3>{title}</h3>
             <div>
-                <input />
+                <input/>
                 <button>+</button>
             </div>
-            <ul>
-                {
-                    !tasks.length
-                    ? <div>EMPTY</div>
-                    :tasks.map((task) => {
-                    return (
-                        <li key={task.id}>
-                            <button onClick={()=>removeTask(task.id)}>x</button>
-                            <input type="checkbox" checked={task.isDone}/>
-                            <span>{task.title}</span>
-                        </li>
-                    )
-                })}
+            {
+                tasks.length === 0
+                    ? <p>EMPTY</p>
+                    : <ul>
+                        {funCompleted.map((task) => {
+                            return (
+                                <li key={task.id}>
+                                    <button onClick={() => removeTask(task.id)}>x</button>
+                                    <input type="checkbox" checked={task.isDone}/>
+                                    <span>{task.title}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+            }
 
-            </ul>
             <div>
-                <button onClick={()=>filterTasks('All')}>'All'</button>
-                <button onClick={()=>filterTasks('Active')}>'Active'</button>
-                <button onClick={()=>filterTasks('Completed')}>'Completed'</button>
+                <button onClick={() => filterTasks('All')}>'All'</button>
+                <button onClick={() => filterTasks('Active')}>'Active'</button>
+                <button onClick={() => filterTasks('Completed')}>'Completed'</button>
             </div>
         </div>
     )
